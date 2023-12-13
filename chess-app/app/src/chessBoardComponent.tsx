73d281cc81d16c86f970e6ab35275ffce142a03a
Nathan Voong <nathan.voong@clearscore.com>
2023-12-13 00:20:53 +0000

    add feature to allow pieces to move (partially working)

--- chess-app/app/src/chessBoardComponent.tsx
+++ chess-app/app/src/chessBoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "../App.css";
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
@@ -11,6 +11,27 @@ interface TileProps {
     isHighlighted: boolean;
 }
 
+//converts [x, y] co-ordinates to pinpoint tiles in the board array
+function gridPosition(x: number, y: number): number {
+    const adjustedY = (y - 7) * -1;
+
+    if (adjustedY > 0) {
+        return (adjustedY * 8) + x;
+    } else {
+        return x + adjustedY;
+    }
+}
+function reverseGridPosition(index: number): { x: number; y: number } {
+    const adjustedY = Math.floor(index / 8);
+
+    if (adjustedY > 0) {
+        return { x: index % 8, y: 7 - adjustedY };
+    } else {
+        return { x: index % 8, y: 7 + adjustedY };
+    }
+}
+
+
 function Tile({ colour, img, onClick, isHighlighted }: TileProps) {
     const baseTileClassName = `tile ${colour}-tile`;
     const validTileClassName = `tile valid-${colour}-tile`;
@@ -22,10 +43,14 @@ function Tile({ colour, img, onClick, isHighlighted }: TileProps) {
     };
 
     if (!img) {
-        return <div className={baseTileClassName}>
-            <button className={validTileClassName} onClick={handleClick}>
-            </button>
-        </div>;
+        return (
+            <div className={baseTileClassName}>
+                <button className="tile valid-tile-button" onClick={handleClick}>
+                    <div className={validTileClassName}></div>
+                </button>
+
+            </div>
+        )
     } else {
         return (
             <div>
@@ -39,11 +64,11 @@ function Tile({ colour, img, onClick, isHighlighted }: TileProps) {
 
 export default function Chessboard() {
     const [highlightedTile, setHighlightedTile] = useState<{ x: number; y: number } | null>(null);
-
+    const [board, setBoard] = useState<JSX.Element[]>([]);
     const createRow = (pieces: string[], colour: string, rowIndex: number): JSX.Element[] => {
         return pieces.map((piece, i) => (
             <Tile
-                key={i}
+                key={gridPosition(i, rowIndex)}
                 colour={(i % 2 === 0) ? colour : (colour === "white" ? "black" : "white")}
                 img={`assets/images/${piece}`}
                 onClick={() => handleTileClick(i, rowIndex)}
@@ -55,7 +80,7 @@ export default function Chessboard() {
     const createPawns = (colour: string, rowIndex: number): JSX.Element[] => {
         return horizontalAxis.map((_, i) => (
             <Tile
-                key={i}
+                key={gridPosition(i, rowIndex)}
                 colour={(i % 2 === 0) ? colour : (colour === "white" ? "black" : "white")}
                 img={`assets/images/${colour}Pawn.png`}
                 onClick={() => handleTileClick(i, rowIndex)}
@@ -67,7 +92,7 @@ export default function Chessboard() {
     const createEmptyRow = (rowIndex: number): JSX.Element[] => {
         return horizontalAxis.map((_, i) => (
             <Tile
-                key={i}
+                key={gridPosition(i, rowIndex)}
                 colour={(rowIndex + i) % 2 === 0 ? 'black' : 'white'}
                 onClick={() => handleTileClick(i, rowIndex)}
                 isHighlighted={highlightedTile && highlightedTile.x === i && highlightedTile.y === rowIndex}
@@ -79,33 +104,73 @@ export default function Chessboard() {
         // Check if the clicked tile is already highlighted
         if (highlightedTile && highlightedTile.x === x && highlightedTile.y === y) {
             // If it is, unhighlight the tile by setting highlightedTile to null
+            console.log("1")
             setHighlightedTile(null);
-        } else {
+        }
+        else {
             // If it's not highlighted, set the clicked tile as highlighted
-            setHighlightedTile({ x, y });
+            if (highlightedTile) {
+                if (board[gridPosition(x,y)].props.img) {
+                    console.log("2")
+                    setHighlightedTile({ x, y });
+                } else {
+                    console.log("3")
+                    movePiece(board, highlightedTile, 0, 16)
+                    movePiece(board, highlightedTile, gridPosition(highlightedTile.x, highlightedTile.y), gridPosition(x, y))
+                }
+            } else {
+                console.log('4')
+                setHighlightedTile({ x, y });
+            }
         }
     };
+    function movePiece(board: JSX.Element[], highlightedTile: { x: any; y: any; }, currentTile: number, targetTile: number) {
+        const updatedBoard = [...board];
 
-    const board: JSX.Element[] = [];
+        updatedBoard[updatedBoard[targetTile].key] = <Tile
+            key={updatedBoard[targetTile].key}
+            colour={updatedBoard[targetTile].props.colour}
+            img={updatedBoard[currentTile].props.img}
+            onClick={updatedBoard[targetTile].props.onClick}
+            isHighlighted={highlightedTile && highlightedTile.x === reverseGridPosition(+updatedBoard[targetTile].key).x && highlightedTile.y === reverseGridPosition(+updatedBoard[targetTile].key).y}
+        />
 
-    for (let j = verticalAxis.length - 1; j >= 0; j--) {
-        if (j === 7) {
-            board.push(...createRow(['blackRook.png', 'blackKnight.png', 'blackBishop.png', 'blackQueen.png', 'blackKing.png', 'blackBishop.png', 'blackKnight.png', 'blackRook.png'], 'white', j));
-        } else if (j === 6) {
-            board.push(...createPawns('black', j));
-        } else if (j === 0) {
-            board.push(...createRow(['whiteRook.png', 'whiteKnight.png', 'whiteBishop.png', 'whiteQueen.png', 'whiteKing.png', 'whiteBishop.png', 'whiteKnight.png', 'whiteRook.png'], 'black', j));
-        } else if (j === 1) {
-            board.push(...createPawns('white', j));
-        } else {
-            board.push(...createEmptyRow(j));
-        }
+        updatedBoard[updatedBoard[currentTile].key] = <Tile
+            key={updatedBoard[currentTile].key}
+            colour={updatedBoard[currentTile].props.colour}
+            img={null}
+            onClick={updatedBoard[currentTile].props.onClick}
+            isHighlighted={highlightedTile && highlightedTile.x === reverseGridPosition(+updatedBoard[currentTile].key).x && highlightedTile.y === reverseGridPosition(+updatedBoard[currentTile].key).y}
+        />
+
+        setBoard(updatedBoard);
     }
 
+    useEffect(() => {
+        let newBoard: JSX.Element[] = [];
+
+        for (let j = verticalAxis.length - 1; j >= 0; j--) {
+            if (j === 7) {
+                newBoard.push(...createRow(['blackRook.png', 'blackKnight.png', 'blackBishop.png', 'blackQueen.png', 'blackKing.png', 'blackBishop.png', 'blackKnight.png', 'blackRook.png'], 'white', j));
+            } else if (j === 6) {
+                newBoard.push(...createPawns('black', j));
+            } else if (j === 0) {
+                newBoard.push(...createRow(['whiteRook.png', 'whiteKnight.png', 'whiteBishop.png', 'whiteQueen.png', 'whiteKing.png', 'whiteBishop.png', 'whiteKnight.png', 'whiteRook.png'], 'black', j));
+            } else if (j === 1) {
+                newBoard.push(...createPawns('white', j));
+            } else {
+                newBoard.push(...createEmptyRow(j));
+            }
+        }
+
+        setBoard(newBoard);
+    }, [highlightedTile]);
+    //movePiece(board, highlightedTile, 0, 20)
     return (
         <>
             <div id="chessboard">{board}</div>
-            <div id="chessboard">{highlightedTile ? `Highlighted Tile: (${highlightedTile.x}, ${highlightedTile.y})` : 'No Tile Highlighted'}</div>
+            <div id="chessboard">{highlightedTile ? board[gridPosition(highlightedTile.x, highlightedTile.y)] : 'No Tile Highlighted'}</div>
+            <div id="chessboard">{highlightedTile ? [highlightedTile.x, highlightedTile.y] : null}</div>
         </>
     );
 }
