aa63c68ec091bc21871921f934fda03d266f643d
Nathan Voong <nathan.voong@clearscore.com>
2024-01-06 17:18:11 +0000

    improved initialising board state

--- chess-app/app/src/Components/Chessboard/chessboard.tsx
+++ chess-app/app/src/Components/Chessboard/chessboard.tsx
@@ -1,8 +1,38 @@
-import React from "react";
 import "./Chessboard.css";
+import Tile from "../Tile/tile";
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+interface Piece {
+    image: string;
+    x: number;
+    y: number;
+}
+
+const pieces: Piece[] = [];
+
+for (let p = 0; p < 2; p++) {
+    const type = (p === 0) ? "black" : "white";
+    const y = (p === 0) ? 7 : 0;
+
+    pieces.push({image: `assets/images/${type}Rook.png`, x: 0, y: y })
+    pieces.push({image: `assets/images/${type}Knight.png`, x: 1, y: y })
+    pieces.push({image: `assets/images/${type}Bishop.png`, x: 2, y: y })
+    pieces.push({image: `assets/images/${type}Queen.png`, x: 3, y: y })
+    pieces.push({image: `assets/images/${type}King.png`, x: 4, y: y })
+    pieces.push({image: `assets/images/${type}Bishop.png`, x: 5, y: y })
+    pieces.push({image: `assets/images/${type}Knight.png`, x: 6, y: y })
+    pieces.push({image: `assets/images/${type}Rook.png`, x: 7, y: y })
+}
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({image: "assets/images/blackPawn.png", x: i, y:6})
+}
+
+for (let i = 0; i < 8; i++) {
+    pieces.push({image: "assets/images/whitePawn.png", x: i, y:1})
+}
 
 export default function Chessboard () {
     let board = [];
@@ -10,16 +40,15 @@ export default function Chessboard () {
     for (let j = verticalAxis.length - 1; j >= 0; j--) {
         for (let i = 0; i < horizontalAxis.length; i++) {
             const number = j + i + 2;
+            let image = undefined;
 
-            if (number % 2 === 0) {
-                board.push(
-                    <div className="tile black-tile"></div>
-                )
-            } else {
-                board.push(
-                    <div className="tile white-tile"></div>
-                )
-            }
+            pieces.forEach(p => {
+                if (p.x === i && p.y === j) {
+                    image = p.image
+                }
+            })
+
+            board.push(<Tile image={image} number={number}/>)
         }
     }
 
